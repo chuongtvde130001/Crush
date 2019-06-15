e12734672c202138de4e313ce38f488409924673
chuongtvde130001 <chuongtvde130001@fpt.edu.vn>
2019-06-15 17:48:48 +0700

    hh

--- Assignment/src/ws/Test.java
+++ Assignment/src/ws/Test.java
@@ -12,7 +12,7 @@ public class Test {
         System.out.println((new JsonParser().parse(s)).getAsJsonObject().get("id"));
     }
     public static void main(String args[]){
-        Gson g = new Gson();
+        Gson g = new Gson();//
         System.out.println((new JsonParser().parse("{'id':'clien t1'}")).getAsJsonObject().get("id"));
         Message mes = new Message(1,2,"aaaa");
         System.out.println(g.toJson(mes));
