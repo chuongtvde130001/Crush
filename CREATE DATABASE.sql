f2eddfa411f89b2fe74d4c6b6098da0272ff1238
chuongtvde130001 <chuongtvde130001@fpt.edu.vn>
2019-06-29 10:43:23 +0700

    DB 29/6

--- CREATE DATABASE.sql
+++ CREATE DATABASE.sql
@@ -9,6 +9,8 @@ CREATE TABLE USERS(
 	Gender char(6), --MALE | FEMALE | LGBT
 	Email varchar(30),
 	Avatar varchar(MAX),
+	ProfileImage varchar(MAX),
+	Description nvarchar(300),
 	LastLogin DateTime,
 	Status int, --1 IS INACTIVE EMAIL | 2 IS INACTIVE USERINFO | 0 IS OK
 	UserRight int -- 1,2,3 (3 is user,2 is staff, 1 is admin
@@ -41,19 +43,51 @@ CREATE TABLE MESSAGES(
 	Time DateTime,
 	Status bit -- false IS UNREADED | true IS READED
 )
-
-INSERT INTO Users(UserName,Password,FullName,Age,Gender,Email,Status,UserRight) VALUES ('tomcat123','81dc9bdb52d04dc20036dbd8313ed055','Tom Allen',18,'MALE','a&b.c',2,3)
-INSERT INTO Users(UserName,Password,FullName,Age,Gender,Email,Status,UserRight) VALUES ('tomcat456','81dc9bdb52d04dc20036dbd8313ed055','Tom Alex',18,'MALE','a&b.c',2,3)
+--FAKE INFO PASS 1234
+INSERT INTO Users(UserName,Password,FullName,Age,Gender,Email,Status,UserRight,Avatar) VALUES ('tomcat123','81dc9bdb52d04dc20036dbd8313ed055','Tom Allen',18,'MALE','a&b.c',2,3,'avatars/2.png')
+INSERT INTO Users(UserName,Password,FullName,Age,Gender,Email,Status,UserRight,Avatar) VALUES ('tomcat456','81dc9bdb52d04dc20036dbd8313ed055','Tom Alex',18,'MALE','a&b.c',2,3,'avatars/2.png')
+INSERT INTO Users(UserName,Password,FullName,Age,Gender,Email,Status,UserRight,Avatar) VALUES ('tomcat789','81dc9bdb52d04dc20036dbd8313ed055','Harry',18,'FEMALE','a&b.c',2,3,'avatars/2.png')
 
 INSERT INTO FRIENDS(UserA,UserB,FR) Values (1,2,getdate())
-
-INSERT INTO MESSAGES (FID,FR,Content,Time,Status) VALUES (1,1,'hello',getdate(),0)
+INSERT INTO FRIENDS(UserA,UserB,FR) Values (3,1,getdate())
+--FAKE INFO
 
 DELETE FROM MESSAGES
 
 SELECT * FROM MESSAGES
 
 SELECT * FROM USERS
+
+SELECT * FROM FRIENDS
+
+--Friend
+SELECT * FROM USERS WHERE UID != 1 and UID in (
+(
+SELECT UserA FROM FRIENDS WHERE UserB = 1)
+UNION
+(SELECT UserB FROM FRIENDS WHERE UserA = 1)
+)
+--get FID
+SELECT
+CASE
+    WHEN UserA != 1 THEN UserA
+    ELSE UserB
+END
+FROM FRIENDS
+WHERE FID = 1
+
+--get Friends
+SELECT FID, USERS.UID, USERS.FullName, USERS.Avatar FROM
+(SELECT FID,
+CASE
+    WHEN UserA != 1 THEN UserA
+    ELSE UserB
+END AS UID
+FROM FRIENDS
+WHERE UserA = 1 OR UserB = 1
+) AS R1
+LEFT JOIN USERS ON (R1.UID = USERS.UID)
+
 
 --SELECT * FROM USERS
 --SELECT USER LOGIN
